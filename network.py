d57f3fd7c3ae84f6d6b74e2c2f267f3742eb2395
Austin McKee <amckee3@gmail.com>
2015-09-23 18:24:33 -0700

    asdf

--- network.py
+++ network.py
@@ -1,1 +1,35 @@
-print("hello world")
+import socket
+import sys
+ 
+HOST = ''   # Symbolic name, meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("socket created")
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print("Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
+    sys.exit()
+     
+print("Socket bind complete")
+ 
+#Start listening on socket
+s.listen(10)
+print("Socket now listening")
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print("Connected with " + addr[0] +":" + str(addr[1]))
+    conn.sendto("this", addr)
+    info = s.recv(4096)
+    print(info)
+    #s.sendall(info)
+
+     
+     
+s.close()
